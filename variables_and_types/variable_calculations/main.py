d879bfe54d63be346922c3968cacee19ebaab86a
gigov13 <>
2026-02-11 11:43:51 +0000

    Variable calculations

---
+++ variables_and_types/variable_calculations/main.py
@@ -0,0 +1,10 @@
+
+item_name = "Soda"
+item_price = 6.99
+item_count = 5
+items_per_case = 1
+purchase_quantity = item_count*items_per_case
+total_cost = item_price*item_count
+
+# Testing
+print("Total cost for", purchase_quantity, "cases of", item_name, "is $", total_cost)
