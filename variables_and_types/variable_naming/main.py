e67b68da450b911ff0cc7b2c28a82c04c1ab5914
gigov13 <>
2026-02-11 11:34:17 +0000

    Variable Naming

---
+++ variables_and_types/variable_naming/main.py
@@ -0,0 +1,9 @@
+
+item_name = "Bread"
+price = 4.52
+quantity_in_stock = 230
+
+# Testing
+print("Item:", item_name)
+print("Price per loaf: $", price)
+print("Quantity in stock:", quantity_in_stock)
